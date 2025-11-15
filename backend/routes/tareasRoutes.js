0ebe3b3d39c9c1f0daf5b10e25794adaf62d1405
squatharp <carlosalexisgutisarm@gmail.com>
2025-11-14 19:23:56 -0600

    Version Nueva

--- backend/routes/tareasRoutes.js
+++ backend/routes/tareasRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const {getTareas, createTareas, updateTareas, deleteTareas} = require ("../controllers/tareasControllers")
 const {protect} = require("../middleware/authMiddleware")
 
-router.get("/", getTareas)
-router.post("/", createTareas)
-router.put("/:id", updateTareas)
-router.delete("/:id", deleteTareas)
+router.get("/", protect, getTareas)
+router.post("/", protect, createTareas)
+router.put("/:id", protect, updateTareas)
+router.delete("/:id",protect,  deleteTareas)
 
 module.exports= router
